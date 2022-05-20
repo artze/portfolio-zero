f3899b49379bf9c18c24573f508c777035253ea2
artze <arthurwtm@gmail.com>
2022-05-20 13:53:21 +0800

    Add grid control

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,28 @@
 
 <div class="container">
 	<div class="grid">
+		<!-- Grid Control -->
+		<div class="control-cell pos1-1" />
+		<div class="control-cell pos2-1" />
+		<div class="control-cell pos3-1" />
+		<div class="control-cell pos4-1" />
+		<div class="control-cell pos5-1" />
+		<div class="control-cell pos1-2" />
+		<div class="control-cell pos2-2" />
+		<div class="control-cell pos3-2" />
+		<div class="control-cell pos4-2" />
+		<div class="control-cell pos5-2" />
+		<div class="control-cell pos1-3" />
+		<div class="control-cell pos2-3" />
+		<div class="control-cell pos3-3" />
+		<div class="control-cell pos4-3" />
+		<div class="control-cell pos5-3" />
+		<div class="control-cell pos1-4" />
+		<div class="control-cell pos2-4" />
+		<div class="control-cell pos3-4" />
+		<div class="control-cell pos4-4" />
+		<div class="control-cell pos5-4" />
+
 		<div class="brand-area">
 			<span class="brand-title">Arthur W</span>
 		</div>
