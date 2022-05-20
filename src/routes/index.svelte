1e51ce3029dd7efeb6a3a85727c25e8ce843f7ad
artze <arthurwtm@gmail.com>
2022-05-20 21:14:27 +0800

    Add decorative lines

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,9 @@
 	<span class="home__text">Software Engineer</span>
 	<span class="home__text">Designer</span>
 </div>
+<div class="decorative-horizontal">
+	<div class="decorative-horizontal__line" />
+</div>
+<div class="decorative-vertical">
+	<div class="decorative-vertical__line" />
+</div>
