264206c0a50fc7824493c65d15afe639fe1cdb21
artze <arthurwtm@gmail.com>
2022-05-20 20:27:38 +0800

    Split containers

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,34 +4,30 @@
 </script>
 
 <div class="container">
-	<div class="grid">
-		<!-- Grid Control -->
-		<div class="control-cell pos1-1" />
-		<div class="control-cell pos2-1" />
-		<div class="control-cell pos3-1" />
-		<div class="control-cell pos4-1" />
-		<div class="control-cell pos5-1" />
-		<div class="control-cell pos1-2" />
-		<div class="control-cell pos2-2" />
-		<div class="control-cell pos3-2" />
-		<div class="control-cell pos4-2" />
-		<div class="control-cell pos5-2" />
-		<div class="control-cell pos1-3" />
-		<div class="control-cell pos2-3" />
-		<div class="control-cell pos3-3" />
-		<div class="control-cell pos4-3" />
-		<div class="control-cell pos5-3" />
-		<div class="control-cell pos1-4" />
-		<div class="control-cell pos2-4" />
-		<div class="control-cell pos3-4" />
-		<div class="control-cell pos4-4" />
-		<div class="control-cell pos5-4" />
-
-		<div class="brand-area">
-			<span class="brand-title">Arthur W</span>
+	<div class="main">
+		<div class="main__grid">
+			<div class="control-cell pos1-1" />
+			<div class="control-cell pos2-1" />
+			<div class="control-cell pos3-1" />
+			<div class="control-cell pos1-2" />
+			<div class="control-cell pos2-2" />
+			<div class="control-cell pos3-2" />
+			<div class="control-cell pos1-3" />
+			<div class="control-cell pos2-3" />
+			<div class="control-cell pos3-3" />
+			<div class="control-cell pos1-4" />
+			<div class="control-cell pos2-4" />
+			<div class="control-cell pos3-4" />
+			<slot />
 		</div>
-		<slot />
-		<div class="art-area" />
-		<div class="nav-area" />
+	</div>
+	<div class="art" />
+	<div class="nav">
+		<div class="nav__grid">
+			<div class="control-cell pos1-1" />
+			<div class="control-cell pos1-2" />
+			<div class="control-cell pos1-3" />
+			<div class="control-cell pos1-4" />
+		</div>
 	</div>
 </div>
--- src/routes/index.svelte
+++
@@ -1,4 +0,0 @@
-<div class="brand-subtitle-area">
-	<span class="brand-subtitle">Software Engineer</span>
-	<span class="brand-subtitle">Designer</span>
-</div>
