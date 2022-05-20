bef48430b9b79694c626b82f9b441cc80ebb4b88
artze <arthurwtm@gmail.com>
2022-05-20 20:33:49 +0800

    Add brand and home text

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,6 +6,7 @@
 <div class="container">
 	<div class="main">
 		<div class="main__grid">
+			<!-- Grid Control -->
 			<div class="control-cell pos1-1" />
 			<div class="control-cell pos2-1" />
 			<div class="control-cell pos3-1" />
@@ -18,12 +19,17 @@
 			<div class="control-cell pos1-4" />
 			<div class="control-cell pos2-4" />
 			<div class="control-cell pos3-4" />
+
+			<div class="brand">
+				<span class="brand__text">Arthur W</span>
+			</div>
 			<slot />
 		</div>
 	</div>
 	<div class="art" />
 	<div class="nav">
 		<div class="nav__grid">
+			<!-- Grid Control -->
 			<div class="control-cell pos1-1" />
 			<div class="control-cell pos1-2" />
 			<div class="control-cell pos1-3" />
---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<div class="home">
+	<span class="home__text">Software Engineer</span>
+	<span class="home__text">Designer</span>
+</div>
